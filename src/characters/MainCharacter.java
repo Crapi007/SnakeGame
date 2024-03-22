b9bd8643c2f6f111cc4e146e6fd364bf17a6bd7e
Craapi <57094175+Crapi007@users.noreply.github.com>
2024-03-22 15:55:32 +0100

    Update MainCharacter.java

--- src/characters/MainCharacter.java
+++ src/characters/MainCharacter.java
@@ -7,19 +7,7 @@ import javax.swing.JPanel;
 
 public class MainCharacter extends JPanel {
 	
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
-	
-	final int CHARACTERHEIGHT = 100;
-	final int CHARACTERWIDTH = 100;
-	
-	protected void paintComponent(Graphics g) {
-		
-		super.paintComponent(g);
-		
-		g.drawRect(200, 700, CHARACTERWIDTH, CHARACTERHEIGHT);
+	// Not yet implemented
 		
 	}
 
