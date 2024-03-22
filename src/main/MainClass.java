00ed252555fa6582ec98840aeeb53936188be6f1
Craapi <57094175+Crapi007@users.noreply.github.com>
2024-03-22 21:29:57 +0100

    Update MainClass.java

--- src/main/MainClass.java
+++ src/main/MainClass.java
@@ -5,7 +5,8 @@ import gui.GameWindow;
 public class MainClass {
 
 	public static void main(String[] args) {
-		
+
+		// Create Instance of the Game Window
 		new GameWindow(600, 600, "Snake");
 		
 	}
